d9a3f094a6674210ddbf4fa3f828f52c6cea0e79
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-17 16:25:48 +0200

    dev: converted to ts with typings

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,7 +1,7 @@
 console.log("About to start the server...");
 
-const express = require("express");
-const serveIndex = require("serve-index");
+import express from "express";
+import serveIndex from "serve-index";
 
 const app = express();
 const port = 3000;
