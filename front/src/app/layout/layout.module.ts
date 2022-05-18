566f62820b32bd2b4a6dd84a1aab7dfc19baa5fd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-18 12:14:43 +0200

    dev: routing step3
    Adding router module to layout module.
    Using router-outlet directive in main component.

--- front/src/app/layout/layout.module.ts
+++ front/src/app/layout/layout.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent, MainComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [HeaderComponent, MainComponent, FooterComponent],
 })
 export class LayoutModule {}
