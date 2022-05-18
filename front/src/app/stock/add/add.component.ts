39d305088a247b20cf4670302897542603a3521a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-18 17:31:33 +0200

    dev: started to handle form
    ajouter ReactFormModule
    declarer le formGroup + liaison avec HTML

--- front/src/app/stock/add/add.component.ts
+++ front/src/app/stock/add/add.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
 export class AddComponent implements OnInit {
+  f = new FormGroup({
+    name: new FormControl('Tournevis', [Validators.required]),
+    price: new FormControl(10, [Validators.required]),
+    qty: new FormControl(1, [Validators.required]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
