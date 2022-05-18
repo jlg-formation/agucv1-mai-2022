20d9b65cf9f5b607ee0b32c203088757fd886a6f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-18 16:33:20 +0200

    dev: added *ngFor for listing articles
    Now articles comes from a JS object.

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Articles } from '../interfaces/article';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  articles: Articles = [
+    {
+      name: 'Tournevis cruciforme',
+      price: 2.34,
+      qty: 103,
+    },
+    {
+      name: 'Pelle',
+      price: 6,
+      qty: 15,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
