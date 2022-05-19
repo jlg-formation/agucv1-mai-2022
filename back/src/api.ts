2aa7a03d109a2810f5e96b9ad625dd58c34ee172
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-19 15:22:09 +0200

    dev: added get articles on back-end

--- back/src/api.ts
+++ back/src/api.ts
@@ -1,4 +1,10 @@
 import { Router } from "express";
+import { Articles } from "./interfaces/article";
+
+const articles: Articles = [
+  { name: "Pinceaux", price: 4.5, qty: 100 },
+  { name: "Marteau", price: 2.5, qty: 40 },
+];
 
 const app = Router();
 
@@ -6,6 +12,10 @@ app.get("/date", (req, res) => {
   res.json({
     date: new Date(),
   });
+});
+
+app.get("/articles", (req, res) => {
+  res.json(articles);
 });
 
 export const api = app;
---
+++ back/src/interfaces/article.ts
@@ -0,0 +1,7 @@
+export interface Article {
+  name: string;
+  price: number;
+  qty: number;
+}
+
+export type Articles = Article[];
