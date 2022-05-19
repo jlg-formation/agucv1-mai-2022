b88e217bd3bf4a49922960010bedf61e10b30389
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-19 15:21:42 +0200

    dev: added cors on back-end

--- back/src/server.ts
+++ back/src/server.ts
@@ -2,6 +2,8 @@ console.log("About to start the server...");
 
 import express from "express";
 import serveIndex from "serve-index";
+import cors from "cors";
+
 import { api } from "./api";
 
 const app = express();
@@ -11,6 +13,8 @@ app.use((req, res, next) => {
   console.log("req.url: ", req.url);
   next();
 });
+
+app.use(cors());
 
 app.use("/api", api);
 
