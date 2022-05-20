ae5adc60296ea304dc84fdf724897eea9dbd9887
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-20 15:36:39 +0200

    e2e: functional test

--- front/cypress/integration/spec.ts
+++ front/cypress/integration/spec.ts
@@ -1,7 +1,26 @@
+const uuid = () => Cypress._.random(0, 1e6);
+
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('Welcome')
-    cy.contains('sandbox app is running!')
-  })
-})
+    cy.visit('/');
+    cy.contains('Gestion Stock');
+    cy.contains('Gérer votre stock efficacement !');
+
+    cy.get('button').contains('Voir le stock').click();
+
+    cy.get('button[title="Ajouter"]').click();
+
+    cy.contains("Ajout d'un article");
+
+    const id = uuid();
+    cy.get('input[formcontrolname="name"]').clear().type(`Machine ${id}`);
+    cy.get('input[formcontrolname="price"]').clear().type('12');
+    cy.get('input[formcontrolname="qty"]').clear().type('13');
+
+    cy.get('button').click();
+
+    cy.get('table tbody tr:last-child').contains(
+      `Machine ${id}`.substring(0, 13)
+    );
+  });
+});
