58f58eecfeec668def6ec8fa5883b6d6b28dbe39
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-20 10:38:18 +0200

    test: all tests pass

--- front/src/app/services/http-article.service.spec.ts
+++ front/src/app/services/http-article.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { HttpArticleService } from './http-article.service';
@@ -6,7 +7,9 @@ describe('HttpArticleService', () => {
   let service: HttpArticleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(HttpArticleService);
   });
 
