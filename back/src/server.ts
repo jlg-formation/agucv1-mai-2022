b5728358d2cd177aee798f20069b242fb98af53a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-20 16:09:47 +0200

    prod: setup wwwDir to angular prod directory

--- back/src/server.ts
+++ back/src/server.ts
@@ -8,6 +8,7 @@ import { api } from "./api";
 
 const app = express();
 const port = 3000;
+const wwwDir = "../front/dist/front";
 
 app.use((req, res, next) => {
   console.log("req.url: ", req.url);
@@ -18,8 +19,8 @@ app.use(cors());
 
 app.use("/api", api);
 
-app.use(express.static("."));
-app.use(serveIndex(".", { icons: true }));
+app.use(express.static(wwwDir));
+app.use(serveIndex(wwwDir, { icons: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
